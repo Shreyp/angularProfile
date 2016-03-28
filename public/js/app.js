b80aa05b466960d7416c59816a1146315aecaf32
Shreyans Patel <Shreyans Patel>
2016-03-28 16:24:11 -0400

    created blog page and extension

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,27 @@
-angular.module('profileApp', ['ui.router'])
-.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-  $urlRouterProvider.otherwise('/home');
-
+angular.module('profileApp',['ui.router'])
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
+  // $urlRouterProvider.otherwise('/');
   $stateProvider
   .state('home', {
-    url: '/home',
-    templateUrl: '/views/home.html'
-  });
+    url: '/',
+    templateUrl: '/views/home.html', 
+    controller: 'homecontroller',
+})
+  .state('login', {
+    url: '/login',
+    templateUrl: '/views/login.html', 
+    controller: 'logincontroller',
+})
+  .state('blog', {
+    url: '/blog',
+    templateUrl: '/views/blog.html',
+    controller: 'blogcontroller'
+});
+    
 
   $locationProvider.html5Mode(true);
-});
+
+  }]);
+
+
+
