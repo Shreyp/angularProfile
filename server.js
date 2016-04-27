82e28276c01717c2c635015e8a4ac49969e65832
shreyp <shreyansap@gmail.com>
2016-04-26 23:00:11 -0400

    added cors functionality

--- server.js
+++ server.js
@@ -39,6 +39,14 @@ db.once('open', function() {
 
 var Message = require('./server/models/messageModel.js');
 
+//allow cors
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
+
 //setup logger
 var logger = require('morgan');
 app.use(logger('dev'));
